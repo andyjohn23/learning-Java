c7b2a77e385e2b3c780a7bd0b2cadee5b893a660
andyjohn23 <andrewnick906@gmail.com>
2023-01-10 19:06:48 +0300

    wrote my first program in java, gross pay calculator.

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,7 +1,23 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+//        Get the number of hours worked
+        int hours = 0;
+        System.out.print("How many hours did you work? ");
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+//          Get the hourly pay rate
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+        payRate = scanner.nextDouble();
+
+        scanner.close();
+//            Multiply hours and pay rate
+        double grossPay = hours * payRate;
+//              Display results
+        System.out.println("Your gross pay is: " + grossPay);
     }
 }
