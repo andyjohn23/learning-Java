36e8b4d051eb1d5239dabfcae6ce9a22701c4e1d
andyjohn23 <andrewnick906@gmail.com>
2023-01-10 20:04:29 +0300

    completed writing my second program in java, blank story program

--- src/fill_in_the_blank_story_program/BlankStoryProgram.java
+++ src/fill_in_the_blank_story_program/BlankStoryProgram.java
@@ -1,5 +1,24 @@
 package fill_in_the_blank_story_program;
 
-public class BlankStoryProgram {
+import java.util.Scanner;
 
+public class BlankStoryProgram {
+    public static void main(String[] args){
+        String adjective = "";
+        System.out.println("Enter any adjective eg. wet");
+        Scanner scanner = new Scanner(System.in);
+        adjective = scanner.next();
+
+        String season = "";
+        System.out.println("Enter any season eg. rainy");
+        season = scanner.next();
+
+        int numberOfCups = 0;
+        System.out.println("Enter number of cups eg. 5");
+        numberOfCups = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("On a " + adjective + " " + season + " day, I drink a minimum of \n" + numberOfCups + " cups of coffee." );
+    }
 }
