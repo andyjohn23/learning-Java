e14a0cc825508884110614d54801bc8c8a1abae6
andyjohn23 <andrewnick906@gmail.com>
2023-01-11 17:55:32 +0300

    completed writing the dollar game question using logical operators and if else if statement in java

--- src/change_dollar_game/ChangeDollarGame.java
+++ src/change_dollar_game/ChangeDollarGame.java
@@ -6,10 +6,11 @@ public class ChangeDollarGame {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 //        set default values first
-        double winValue = 100;
-        double defaultNickels = 5;
-        double defaultDime = 10;
-        double defaultQuarters = 25;
+        double winValue = 1;
+        double defaultPennies = 0.01;
+        double defaultNickels = 0.05;
+        double defaultDime = 0.10;
+        double defaultQuarters = 0.25;
 
 //        Get necessary information from user
         System.out.println("WELCOME TO CHANGE DOLLAR GAME, YOUR GOAL TO ENTER ENOUGH CHANGE TO MAKE EXACTLY $1.");
@@ -28,8 +29,9 @@ public class ChangeDollarGame {
         double convertedNickels = getNickels * defaultNickels;
         double convertedDimes = getDimes * defaultDime;
         double convertedQuarters = getQuarters * defaultQuarters;
+        double convertedPenny = getPennies * defaultPennies;
 
-        double totalValue = getPennies + convertedNickels + convertedDimes + convertedQuarters;
+        double totalValue = convertedPenny + convertedNickels + convertedDimes + convertedQuarters;
 
         if(totalValue == winValue){
             System.out.println("Congratulations, YOU'VE WON\nyour total change was $" + totalValue);
