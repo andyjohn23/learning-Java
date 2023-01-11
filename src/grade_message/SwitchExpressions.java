0f0ace260c1c31ca2420e4da30411aa541954979
andyjohn23 <andrewnick906@gmail.com>
2023-01-11 15:13:56 +0300

    completed writing grade class program using switch expressions

---
+++ src/grade_message/SwitchExpressions.java
@@ -0,0 +1,23 @@
+package grade_message;
+
+import java.util.Scanner;
+
+public class SwitchExpressions {
+    public static void main(String[] args){
+        System.out.println("Enter your letter grade(in uppercase): ");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+        String message = switch (grade){
+            case "A" -> "Excellent job!";
+            case "B" -> "Great job!";
+            case "C" -> "Good job!";
+            case "D" -> "Put more effort!";
+            case "E" -> "Pull up your socks!";
+            default -> "Please enter a correct letter grade eg. A";
+        };
+
+        System.out.println(message);
+    }
+}
