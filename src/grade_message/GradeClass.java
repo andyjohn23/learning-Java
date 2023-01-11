aa7d0cf8bda907598f03e18792ffe9d95ef9b632
andyjohn23 <andrewnick906@gmail.com>
2023-01-11 14:52:58 +0300

    completed writing the grade class program along side learnt switch statement in java

--- src/grade_message/GradeClass.java
+++ src/grade_message/GradeClass.java
@@ -10,11 +10,34 @@ import java.util.Scanner;
 
 public class GradeClass {
     public static void main(String[] args){
-        System.out.println("Enter your letter grade");
+        System.out.println("Enter your letter grade(in uppercase): ");
         Scanner scanner = new Scanner(System.in);
         String grade = scanner.next();
         scanner.close();
 
+        String message;
+        switch (grade){
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+            case "D":
+                message = "You need to work a bit harder!!!";
+                break;
+            case "E":
+                message = "Pull up your socks!!!";
+                break;
+            default:
+                message = "PLEASE ENTER A VALID GRADE eg. A";
+                break;
+        }
+
+        System.out.println(message);
 
     }
 }
