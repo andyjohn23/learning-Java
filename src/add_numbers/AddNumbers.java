3f985d0a2223a23eb6b6c199bc53c4c5accf8235
andyjohn23 <andrewnick906@gmail.com>
2023-01-12 12:23:39 +0300

    finished writing the add number program using do while loop in java

--- src/add_numbers/AddNumbers.java
+++ src/add_numbers/AddNumbers.java
@@ -1,7 +1,31 @@
 package add_numbers;
 
+/*
+DO WHILE loop.
+Write a program that allows user to enter two numbers,
+and then sums up the two numbers. The user should be able to repeat
+this action until they indicate they are done.
+*/
+
+import java.util.Scanner;
+
 public class AddNumbers {
     public static void main(String[] args){
+        int defaultValue = 0;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.println("Enter the first number");
+            double getNUmber1 = scanner.nextDouble();
+
+            System.out.println("Enter the second number");
+            double getNUmber2 = scanner.nextDouble();
+
+            double totalSum = getNUmber1 + getNUmber2;
+            System.out.println("Your answer is: " + totalSum + ".");
+            System.out.println("Would you like to continue?\nPress 1 for yes or 2 for no");
+            defaultValue = scanner.nextInt();
+        }while(defaultValue == 1);
+        scanner.close();
 
     }
 }
