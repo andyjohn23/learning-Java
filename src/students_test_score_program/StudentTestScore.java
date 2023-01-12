7a24aac2693a80c0edf5fe92c333d1c76d886537
andyjohn23 <andrewnick906@gmail.com>
2023-01-12 13:07:13 +0300

    created a new package and class called student test score

---
+++ src/students_test_score_program/StudentTestScore.java
@@ -0,0 +1,13 @@
+package students_test_score_program;
+
+/*
+Nested loops.
+Find the average test score for each student in the class,
+there are 24 students and four tests.
+*/
+
+public class StudentTestScore {
+    public static void main(String[] args){
+
+    }
+}
