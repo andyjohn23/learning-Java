075dc7803e7e4cc98c7b8531c53fa5cadc913ec7
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 11:51:01 +0300

    completed writing the roll die game challenge in java

--- src/roll_die_game/RollDieGame.java
+++ src/roll_die_game/RollDieGame.java
@@ -8,16 +8,28 @@ public class RollDieGame {
         int gameSpace = 20;
         int totalDieNumber = 6;
         int dieRollCount = 5;
+        int currentSpace = 0;
+
+        System.out.println("WELCOME TO THE ROLL DIE GAME!!!. Let's begin.");
 
         Random random = new Random();
         int rollDie = random.nextInt(totalDieNumber) + 1;
-        int randomGameSpace = random.nextInt(gameSpace) + 1;
+        int randomGameSpace = currentSpace + rollDie;
         int getSpaceRemain = gameSpace - randomGameSpace;
 
-        for(int i=0; i < dieRollCount; i++){
-            if(rollDie == 6 && randomGameSpace == 20){
+        for(int i=1; i < dieRollCount; i++){
+            if(randomGameSpace == 20){
                 System.out.println("You've rolled a " + rollDie + "." + " You're on space " + randomGameSpace + "." + " congrats you win.");
-            }else{
+                break;
+            }else if(randomGameSpace > gameSpace){
+                System.out.println("Unfortunately you have lost the game");
+                break;
+            }else if(i==dieRollCount && randomGameSpace < gameSpace){
+                System.out.println("You're on space " + randomGameSpace + ".");
+                System.out.println("Unfortunately you did not make it to all " + gameSpace + ", you lose!!!");
+                break;
+            }
+            else{
                 System.out.println("You've rolled a " + rollDie + "." + " You're now on space " + randomGameSpace + " and have " + getSpaceRemain + " more to go.");
             }
         }
