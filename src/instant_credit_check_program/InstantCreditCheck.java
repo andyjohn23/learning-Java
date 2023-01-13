ec4dc30828716149605da60eb54ca6dbccf88d2d
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 13:51:37 +0300

    created a new package and class called instant credit check program to learn how to use arguments in java methods

---
+++ src/instant_credit_check_program/InstantCreditCheck.java
@@ -0,0 +1,14 @@
+package instant_credit_check_program;
+
+/*
+Methods in Java.
+Write an instant credit check program that approves anyone,
+who makes more than $25000 and has a credit score of 700 or better.
+Reject all others.
+*/
+
+public class InstantCreditCheck {
+    public static void main(String[] args){
+
+    }
+}
