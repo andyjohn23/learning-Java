7c2f3ac857ee6d07c661fa03ebee7fe13ace5479
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 09:50:25 +0300

    completed writing the letter search program using break statement in java

--- src/letter_search_program/LetterSearch.java
+++ src/letter_search_program/LetterSearch.java
@@ -1,7 +1,31 @@
 package letter_search_program;
 
+import java.util.Scanner;
+
+/*
+Break Statement.
+Write a program that searches for a string to determine,
+if it contains the letter A.
+*/
 public class LetterSearch {
     public static void main(String[] args){
+//        Get text
+        System.out.println("Please enter some text: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        scanner.close();
 
+        boolean foundLetter = false;
+
+//        searching text for letter A
+        for(int i=0; i<text.length(); i++){
+            char currentLetter = text.charAt(i);
+            if(currentLetter == 'A' || currentLetter == 'a'){
+                foundLetter = true;
+                break;
+            }
+        }
+
+        System.out.println("Found letter A?: " + foundLetter);
     }
 }
