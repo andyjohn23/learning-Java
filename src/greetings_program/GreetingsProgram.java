39428c5fecfaa973c15d570b7fc7002225e3ad27
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 13:37:44 +0300

    completed writing the greet user method

--- src/greetings_program/GreetingsProgram.java
+++ src/greetings_program/GreetingsProgram.java
@@ -9,14 +9,13 @@ import java.util.Scanner;
 
 public class GreetingsProgram {
     public static void main(String[] args){
-
+//        calling the greet user method for execution
+        greetUser();
     }
-
     public static void greetUser(){
         System.out.println("Please enter your name");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
-
         System.out.println("Good afternoon " + name + ".");
     }
 }
