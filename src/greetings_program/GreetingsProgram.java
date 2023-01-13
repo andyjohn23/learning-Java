c4c040d448f73b2020f6d2d96ededff93265cb9b
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 13:31:59 +0300

    created a new package and class called greetings program to learn methods in java

---
+++ src/greetings_program/GreetingsProgram.java
@@ -0,0 +1,22 @@
+package greetings_program;
+
+/*
+Methods in Java.
+Write a program that defines a method to greet a user by name.
+*/
+
+import java.util.Scanner;
+
+public class GreetingsProgram {
+    public static void main(String[] args){
+
+    }
+
+    public static void greetUser(){
+        System.out.println("Please enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+        System.out.println("Good afternoon " + name + ".");
+    }
+}
