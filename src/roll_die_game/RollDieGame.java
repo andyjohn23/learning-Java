5f071fd040a69409f8d84f9cda60fdd9951c2df9
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 11:16:01 +0300

    added the for loop to repeat roll 5 times

--- src/roll_die_game/RollDieGame.java
+++ src/roll_die_game/RollDieGame.java
@@ -11,6 +11,16 @@ public class RollDieGame {
 
         Random random = new Random();
         int rollDie = random.nextInt(totalDieNumber) + 1;
+        int randomGameSpace = random.nextInt(gameSpace) + 1;
+        int getSpaceRemain = gameSpace - randomGameSpace;
+
+        for(int i=0; i < dieRollCount; i++){
+            if(rollDie == 6 && randomGameSpace == 20){
+                System.out.println("You've rolled a " + rollDie + "." + " You're on space " + randomGameSpace + "." + " congrats you win.");
+            }else{
+                System.out.println("You've rolled a " + rollDie + "." + " You're now on space " + randomGameSpace + " and have " + getSpaceRemain + " more to go.");
+            }
+        }
 
     }
 }
