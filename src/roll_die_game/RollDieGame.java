0e2251c0491ebc198984a33ee2f6bf7f0181852d
andyjohn23 <andrewnick906@gmail.com>
2023-01-13 10:29:44 +0300

    initialized known values in a variable

--- src/roll_die_game/RollDieGame.java
+++ src/roll_die_game/RollDieGame.java
@@ -1,7 +1,16 @@
 package roll_die_game;
 
+import java.util.Random;
+
 public class RollDieGame {
     public static void main(String[] args){
+//        initialize known values
+        int gameSpace = 20;
+        int totalDieNumber = 6;
+        int dieRollCount = 5;
+
+        Random random = new Random();
+        int rollDie = random.nextInt(totalDieNumber) + 1;
 
     }
 }
